3401c1e3bc440fdc531a0f8b67a0506dd97da27a
bugrahankaramollaoglu <bugrahankaramollaoglu@gmail.com>
2025-06-21 23:10:34 +0300

    navigation initiated. now will try adopting to the main/auth navigation style.

--- Tasty/app/src/main/java/com/bugrahankaramollaoglu/tasty/MainActivity.kt
+++ Tasty/app/src/main/java/com/bugrahankaramollaoglu/tasty/MainActivity.kt
@@ -5,31 +5,72 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val navController = rememberNavController()
             TastyApp(
-                name = "Android",
-                modifier = Modifier
-                    .background(Color.Red)
-                    .fillMaxSize()
+                navController = navController,
             )
         }
     }
 }
 
 @Composable
-fun TastyApp(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TastyApp(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = "home"
+    ) {
+        composable("home") {
+            HomeScreen(navController)
+        }
+        composable("details") {
+            DetailsScreen(navController)
+        }
+    }
+}
+
+
+@Composable
+fun HomeScreen(navController: NavHostController) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Red),
+        contentAlignment = Alignment.Center
+    ) {
+        Button(onClick = { navController.navigate("details") }) {
+            Text(text = "Go to Details Screen")
+        }
+    }
+}
+
+@Composable
+fun DetailsScreen(navController: NavHostController) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Blue),
+        contentAlignment = Alignment.Center
+    ) {
+        Button(onClick = { navController.navigate("home") }) {
+            Text(text = "Go to Home Screen")
+        }
+    }
 }
